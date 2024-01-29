797b1aa503f7979d9d2c5f2bf410536e4bb36676
Mathias <mathias.scroccaro@gmail.com>
2024-01-29 05:00:00 -0300

    Implement the ./cmd files

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,19 +1,9 @@
 package main
 
 import (
-	"github.com/mathiasscroccaro/event-driven-go-query/internal/adapter/kafka"
-	"github.com/mathiasscroccaro/event-driven-go-query/internal/domain"
 	"github.com/mathiasscroccaro/event-driven-go-query/internal/use_case"
 )
 
 func main() {
-
-	kafkaAdapter := kafka.NewKafkaQueueBroker()
-
-	intertEvent := use_case.NewInsertEventInQueueUseCase(kafkaAdapter)
-
-	intertEvent.Execute(domain.WebScrappingEvent{
-		Url:  "https://www.lcsc.com/search?q=C99003",
-		Code: "C99003",
-	})
+	use_case.NewCLIAPIUseCase().Execute()
 }
